7f197ea97751de66352ead456f4532aba8e2ef19
jose david rincon angeles <angelnmara@hotmail.com>
2019-09-11 19:05:32 -0500

    se agrega obten datos from google

--- src/main/java/lamarrulla/com/SEMAFOROV/App.java
+++ src/main/java/lamarrulla/com/SEMAFOROV/App.java
@@ -1,8 +1,15 @@
 package lamarrulla.com.SEMAFOROV;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import lamarrulla.com.SEMAFOROV.NetClientGet;
 /**
  * Hello world!
@@ -11,15 +18,49 @@ import lamarrulla.com.SEMAFOROV.NetClientGet;
 public class App 
 {	
 	static NetClientGet netClientGet = new NetClientGet();
+	static JsonObject jso = new JsonObject();
+	static URL url;
+	
+	
     public static void main( String[] args )
     {    	
+    	try {
+			URL url = new URL("https://maps.googleapis.com/maps/api/js/DirectionsService.Route?5m4&1m3&1m2&1d19.4950119&2d-99.11960449999998&5m4&1m3&1m2&1d19.2800339&2d-99.17037160000001&6e0&12ses-MX&23e1&callback=_xdc_._ft28bq&key=AIzaSyAndp8rBJEaJnxjKdLJV5rfxE8guaZH3Ic&token=106168");
+			netClientGet.setUrl(url);
+			netClientGet.getLocation();
+			arreglaSalidaMaps(netClientGet.salida);
+			JsonArray jsaRoutes = jso.getAsJsonArray("routes");			
+			System.out.println(jsaRoutes.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
     	ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     	executor.scheduleAtFixedRate(helloRunnable, 0, 60, TimeUnit.SECONDS);
     }
-    static Runnable helloRunnable = new Runnable() {
+    private static void arreglaSalidaMaps(String salida) {
+		// TODO Auto-generated method stub
+		salida = salida.replace("/**/_xdc_._ft28bq && _xdc_._ft28bq(", "")
+			.replace("\\u003cb\\u003e", "");
+		salida = salida.substring(0, salida.length()-1);
+		jso = new JsonParser().parse(salida).getAsJsonObject();
+		
+	}
+	static Runnable helloRunnable = new Runnable() {
 	    public void run() {
-	        System.out.println("GetDatos");	    
-	        netClientGet.getLocation();
+	        //System.out.println("GetDatos");	   
+	    	
+			try {
+				URL url = new URL("https://www.waze.com/row-rtserver/web/TGeoRSS?bottom=19.51304459775636&left=-99.28868293762208&ma=0&mj=0&mu=400&right=-99.09092903137207&top=19.55468708780126&types=alerts%2Ctraffic%2Cusers");
+				netClientGet.setUrl(url);
+				netClientGet.getLocation();
+		        //System.out.println(netClientGet.getJso().toString());
+		        System.out.println(netClientGet.salida);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	    		    	       
 	    }
 	};
 }
--- src/main/java/lamarrulla/com/SEMAFOROV/NetClientGet.java
+++ src/main/java/lamarrulla/com/SEMAFOROV/NetClientGet.java
@@ -5,10 +5,35 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class NetClientGet {	
+import com.google.gson.JsonObject;
+
+public class NetClientGet {
+	JsonObject jso;
+	String salida="";
+	URL url;
+	public URL getUrl() {
+		return url;
+	}
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	public String getSalida() {
+		return salida;
+	}
+	public void setSalida(String salida) {
+		this.salida = salida;
+	}
+	public JsonObject getJso() {
+		return jso;
+	}
+	public void setJso(JsonObject jso) {
+		this.jso = jso;
+	}
+	
 	public void getLocation() {
-        try {
-            URL url = new URL("https://www.waze.com/row-rtserver/web/TGeoRSS?bottom=19.51304459775636&left=-99.28868293762208&ma=0&mj=0&mu=400&right=-99.09092903137207&top=19.55468708780126&types=alerts%2Ctraffic%2Cusers");//your url i.e fetch data from .
+        try {        	
+            //URL url = new URL("https://maps.googleapis.com/maps/api/js/DirectionsService.Route?5m4&1m3&1m2&1d19.4950119&2d-99.11960449999998&5m4&1m3&1m2&1d19.2800339&2d-99.17037160000001&6e0&12ses-MX&23e1&callback=_xdc_._ft28bq&key=AIzaSyAndp8rBJEaJnxjKdLJV5rfxE8guaZH3Ic&token=106168");//your url i.e fetch data from .
+            //URL url = new URL("https://www.waze.com/row-rtserver/web/TGeoRSS?bottom=19.51304459775636&left=-99.28868293762208&ma=0&mj=0&mu=400&right=-99.09092903137207&top=19.55468708780126&types=alerts%2Ctraffic%2Cusers");//your url i.e fetch data from .
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Referer", " https://www.waze.com/es/livemap?utm_source=waze_website&utm_campaign=waze_website");
@@ -19,9 +44,10 @@ public class NetClientGet {
             }
             InputStreamReader in = new InputStreamReader(conn.getInputStream());
             BufferedReader br = new BufferedReader(in);
-            String output;
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
+            String output= "";
+            salida = "";
+            while ((output = br.readLine()) != null) {            	
+            	salida += output;          
             }
             conn.disconnect();
 
